cb5ad5e463948007ca747687194e1f6a0e1baf38
Crisandolin Rumahorbo <crisandolin007@gmail.com>
2023-03-16 10:34:12 +0700

    refactor Dashboard page to render TodoList and handle delete from list

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
+import TodoList from "@/components/TodoList";
+import service from "@/pages/service";
 
-export default function DashboardIndexPage() {
+export default function DashboardIndexPage({todoList}) {
+    const [todos, setTodos] = useState(todoList);
+
+    const handleDelete = (id) => {
+        const newTodos = todos.filter(todo => todo.id !== id);
+        setTodos(newTodos);
+    }
+
     return (
-        <div>
-            <h1>Hello World!</h1>
-        </div>
+        <TodoList todos={todos} handleDelete={handleDelete}/>
     )
+}
+
+export async function getServerSideProps() {
+    const todos = await service.getTodos();
+    return {
+        props: {
+            todoList: todos
+        }
+    }
 }
--- test/pages/dashboard/index.test.js
+++ test/pages/dashboard/index.test.js
@@ -1,16 +1,54 @@
 import React from 'react';
-import {render, screen} from "@testing-library/react";
+import {fireEvent, render, screen} from "@testing-library/react";
 import "@testing-library/jest-dom";
-import DashboardIndexPage from "@/pages/dashboard";
+import DashboardIndexPage, {getServerSideProps} from "@/pages/dashboard";
+import axios from "axios";
+import {when} from "jest-when";
+import constant from "@/pages/constant";
+import flushPromises from 'flush-promises';
+
+const {BASE_URL} = constant;
+jest.mock("axios");
 
 describe('Dashboard page', () => {
-    it('should render', () => {
-        render(<DashboardIndexPage />);
+    const data = [
+        {
+            id: 1,
+            title: "learning",
+            isCompleted: true,
+        },
+        {
+            id: 2,
+            title: "swimming",
+            isCompleted: false,
+        }
+    ];
+    when(axios.get)
+        .calledWith(`${BASE_URL}/todo`)
+        .mockResolvedValue({data});
 
-        const header = screen.getByRole('heading', {level: 1});
-        const headerText = "Hello World!";
+    describe('#render', function () {
+        it('should render', async () => {
+            const {props: {todoList}} = await getServerSideProps();
+            render(<DashboardIndexPage todoList={todoList}/>);
+            const title = screen.getAllByTestId("title");
+            await flushPromises();
 
-        expect(header.textContent).toBe(headerText);
+            expect(title).toHaveLength(2);
+        });
+    });
 
+    describe('#handleDelete', function () {
+        it('should called handleDelete', async function () {
+            const {props: {todoList}} = await getServerSideProps();
+            render(<DashboardIndexPage todoList={todoList}/>);
+            const buttonDelete = screen.queryAllByTestId("deleteButton");
+            fireEvent.click(buttonDelete[0]);
+            await flushPromises();
+
+            const status = screen.getAllByTestId("status");
+
+            expect(status).toHaveLength(1);
+        });
     });
 });
