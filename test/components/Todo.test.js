85a753576ebf48c7811f449ea26be875e6267562
Crisandolin Rumahorbo <crisandolin007@gmail.com>
2023-03-16 10:32:12 +0700

    add unit testing for components Todo and TodoList

---
+++ test/components/Todo.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {fireEvent, render, screen} from "@testing-library/react";
+import "@testing-library/jest-dom";
+import TodoComponent from "@/components/Todo";
+
+describe('Todo', function () {
+    beforeEach(function () {
+        jest.resetAllMocks();
+    });
+
+    const deleteButton = jest.fn();
+    const titleContent = "learning";
+    const statusContent = "completed";
+    const todo = {
+        id: 1,
+        title: "learning",
+        isCompleted: true,
+    };
+
+    describe('#render', function () {
+        it('should render title, status and delete button', function () {
+            render(<TodoComponent todo={todo}/>)
+            const title = screen.queryByTestId("title");
+            const status = screen.queryByTestId("status");
+
+            expect(title.textContent).toBe(titleContent);
+            expect(status.textContent).toBe(statusContent);
+        });
+    });
+
+    describe('#delete', function () {
+        it('should called deleteButton when button clicked', function () {
+            render(<TodoComponent todo={todo} handleDelete={deleteButton}/>)
+            fireEvent.click(screen.queryByTestId("deleteButton"));
+
+            expect(deleteButton).toHaveBeenCalled();
+        });
+    });
+});
